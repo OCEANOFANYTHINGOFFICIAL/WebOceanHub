7aca28d244ec0f26cc3d80717bf4ac77059f906f
oceanofanything <oceanofanything@gmail.com>
2021-04-25 18:08:42 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+// for the details tag open
+details.addEventListener("toggle", event => {
+    if (details.open) {
+      /* the element was toggled open */
+    } else {
+      /* the element was toggled closed */
+    }
+  });
+  //for the details tag
