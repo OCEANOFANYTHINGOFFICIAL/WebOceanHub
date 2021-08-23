f3c65f57540f2461564881146e33ec18d574f0bd
oceanofanything <oceanofanything@gmail.com>
2021-08-23 21:23:57 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -7,3 +7,17 @@ details.addEventListener("toggle", event => {
     }
   });
   //for the details tag
+
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
