8ca09850a37b39f483a9c731b93d695925d803b7
oceanofanything <oceanofanything@gmail.com>
2021-08-23 21:38:13 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -7,42 +7,41 @@ details.addEventListener("toggle", event => {
     }
   });
   //for the details tag
+const fireTracking = (label) => {
+  //Fire tracking here
+  console.log(label);
+};
 
-// Initialize deferredPrompt for use later to show browser install prompt.
-let deferredPrompt;
+export const pwaTrackingListeners = () => {
+  const fireAddToHomeScreenImpression = (event) => {
+    fireTracking("Add to homescreen shown");
+    //will not work for chrome, untill fixed
+    event.userChoice.then((choiceResult) => {
+      fireTracking(`User clicked ${choiceResult}`);
+    });
+    //This is to prevent `beforeinstallprompt` event that triggers again on `Add` or `Cancel` click
+    window.removeEventListener(
+      "beforeinstallprompt",
+      fireAddToHomeScreenImpression
+    );
+  };
+  window.addEventListener("beforeinstallprompt", fireAddToHomeScreenImpression);
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-  // Optionally, send analytics event that PWA install promo was shown.
-  console.log(`'beforeinstallprompt' event was fired.`);
-});
-window.addEventListener('appinstalled', () => {
-  // Hide the app-provided install promotion
-  hideInstallPromotion();
-  // Clear the deferredPrompt so it can be garbage collected
-  deferredPrompt = null;
-  // Optionally, send analytics event to indicate successful install
-  console.log('PWA was installed');
-});
-function getPWADisplayMode() {
-  const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
-  if (document.referrer.startsWith('android-app://')) {
-    return 'twa';
-  } else if (navigator.standalone || isStandalone) {
-    return 'standalone';
-  }
-  return 'browser';
-}
-window.matchMedia('(display-mode: standalone)').addEventListener('change', (evt) => {
-  let displayMode = 'browser';
-  if (evt.matches) {
-    displayMode = 'standalone';
-  }
-  // Log display mode change to analytics
-  console.log('DISPLAY_MODE_CHANGED', displayMode);
-});
+  //Track web app install by user
+  window.addEventListener("appinstalled", (event) => {
+    fireTracking("PWA app installed by user!!! Hurray");
+  });
+
+  //Track from where your web app has been opened/browsed
+  window.addEventListener("load", () => {
+    let trackText;
+    if (navigator && navigator.standalone) {
+      trackText = "Launched: Installed (iOS)";
+    } else if (matchMedia("(display-mode: standalone)").matches) {
+      trackText = "Launched: Installed";
+    } else {
+      trackText = "Launched: Browser Tab";
+    }
+    fireTracking(trackText);
+  });
+};
